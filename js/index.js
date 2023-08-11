4bcd02ee8c978ebf8147dcf457c74e81d71f2ab2
JeongMinE2 <alsdl7354@naver.com>
2023-08-11 22:05:19 +0900

    javascript update_1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let logout = document.getElementById("logout");
+let logout_box = document.getElementById("logout_box");
+
+logout.addEventListener("click", function(e) {
+    e.preventDefault();
+    logout_box.style.display = "block";
+    
+})
