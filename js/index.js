8624602bb419f4fd4361c6aeab6193e7ad6ceb2c
JeongMinE2 <alsdl7354@naver.com>
2023-08-12 15:59:18 +0900

    javascript update_2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
 // 로그아웃 버튼 클릭 시 이벤트 발생
-let logout = document.getElementById("logout");
-let logout_box = document.getElementById("logout_box");
+const logout = document.getElementById("logout");
+const logout_box = document.getElementById("logout_box");
 
 logout.addEventListener("click", function(e) {
     e.preventDefault();     // 기본 동작을 실행하지 않도록 지정
     logout_box.style.display = "block";     // logout_box의 스타일을 none에서 block으로 변경
     
 })
+
+// 수강 과목 + 버튼 클릭 시 이벤트 발생
+const plus = document.getElementById("plus");
+const create_subject_box = document.getElementById("create_subject_box");
+
+plus.addEventListener("click", function(e) {
+    e.preventDefault();
+    create_subject_box.style.display = "block";     // logout_box의 스타일을 none에서 block으로 변경
+})
+
+// 로그아웃 취소, 수강 과목 생성 취소 버튼 클릭 시 이벤트 발생
+let cancel = document.querySelectorAll(".cancel");
+
+cancel.forEach((Element) => {
+    Element.addEventListener("click", function(e) {
+        e.preventDefault();
+        logout_box.style.display = "none";
+        create_subject_box.style.display = "none";
+    })
+})
+
 
 // 수강 과목
 const subject = {
@@ -46,13 +67,13 @@ const subject = {
 
 // const subjectData = document.getElementById("data");
 
-function createSubject() {
-    for (subjectList of subject) {
-        // const subjectList = document.createElement("li");
-        // subjectList.innerHTML = `${subjectList.subjectName}`;
-        // subjectData.appendChild(subjectList);
-        let li = document.createElement('li');
-        li.textContent = `${subjectList.subjectName}`;
-    }
-}
-createSubject();
+// function createSubject() {
+//     for (subjectList of subject) {
+//         // const subjectList = document.createElement("li");
+//         // subjectList.innerHTML = `${subjectList.subjectName}`;
+//         // subjectData.appendChild(subjectList);
+//         let li = document.createElement('li');
+//         li.textContent = `${subjectList.subjectName}`;
+//     }
+// }
+// createSubject();
