fd833a813b9e9dab385040b5edfce4d14e1b708f
JeongMinE2 <alsdl7354@naver.com>
2023-08-14 17:34:27 +0900

    javascript update_8

--- js/index.js
+++ js/index.js
@@ -1,118 +1,5 @@
-// 로그아웃 버튼 클릭 시 이벤트 발생
-const logout = document.getElementById("logout");
-const logout_box = document.getElementById("logout_box");
-
-logout.addEventListener("click", function(e) {
-    e.preventDefault();     // 기본 동작을 실행하지 않도록 지정
-    logout_box.style.display = "block";     // logout_box의 스타일을 none에서 block으로 변경
-    
-});
-
-// 수강 과목 + 버튼 클릭 시 이벤트 발생
-const plus = document.getElementById("plus");
-const create_subject_box = document.getElementById("create_subject_box");
-
-plus.addEventListener("click", function(e) {
-    e.preventDefault();
-    create_subject_box.style.display = "block";     // logout_box의 스타일을 none에서 block으로 변경
-});
-
-// 수강 과목 생성에서 생성 버튼 클릭 시 이벤트 발생
-const create = document.getElementById("create");                           // 생성 버튼
-const subject_container = document.getElementById('subject_container');     // 생성 버튼 클릭 시 ul이 추가될 div
-const Ul = subject_container.querySelector('.data');                        // data 클래스 가져오기
-
-create.addEventListener("click", function(e) {
-    e.preventDefault();
-    const newUl = document.createElement('ul');         // 새로운 ul 태그 생성
-    newUl.className = 'data';                           // 새로 생긴 ul 태그는 data 클래스
-    const newLi = Ul.cloneNode(true);                   // Ul(data) 노드 복사, true라서 하위 노드까지 모두 복사
-    create_subject_box.style.display = "none";
-    subject_container.appendChild(newUl);               // subject_container div에 새로운 ul 태그 추가
-    newUl.appendChild(newLi);                           // 새로 추가된 ul에 복제된 노드 추가
-
-    newUl.addEventListener("click", function(e) {
-        const clickX = e.clientX;
-        const clickY = e.clientY;
-
-        setting_button.style.left = clickX + "px";
-        setting_button.style.top = clickY + "px";
-        setting_button.style.display = "block";
-    })
-
-    newLi.addEventListener("click", function(e) {
-        e.stopPropagation();
-    })
-
-    document.addEventListener("click", function(e) {
-        if(e.target !== setting && e.target !== setting_button) {
-            setting_button.style.display = "none";
-        }
-    })
-});
-
-
-// 수강 과목 설정 버튼 클릭 시 이벤트 발생
-const setting = document.querySelector(".setting");
-const setting_button = document.querySelector(".setting_button");
-
-setting.addEventListener("click", function(e) {
-    const clickX = e.clientX;
-    const clickY = e.clientY;
-
-    setting_button.style.left = clickX + "px";
-    setting_button.style.top = clickY + "px";
-    setting_button.style.display = "block";
-});
-
-setting_button.addEventListener("click", function(e) {
-    e.stopPropagation();
-    setting_button.style.display = "none";
-});
-
-// // 설정 버튼이랑 설정 버튼 클릭 시 나오는 레이어 아닌 부분 클릭 시 설정 레이어 사라지는 이벤트
-document.addEventListener("click", function(e) {
-    if(e.target !== setting && e.target !== setting_button) {
-        setting_button.style.display = "none";
-    }
-})
-
-
-// 수정 버튼 클릭 시 이벤트 발생
-const update_button = document.querySelector(".update_button");
-const update_subject_box = document.getElementById("update_subject_box");
-
-update_button.addEventListener("click", function(e) {
-    e.preventDefault();
-    update_subject_box.style.display = "block";       // update_subject_box의 스타일을 none에서 block으로 변경
-});
-
-// 제거 버튼 클릭 시 이벤트 발생
-const delete_button = document.querySelector(".delete_button");
-const delete_box = document.getElementById("delete_box");
-
-delete_button.addEventListener("click", function(e) {
-    e.preventDefault();
-    delete_box.style.display = "block";
-});
-
-
-// 로그아웃 취소, 수강 과목 생성 취소 버튼 클릭 시 이벤트 발생
-let cancel = document.querySelectorAll(".cancel");
-
-cancel.forEach((Element) => {
-    Element.addEventListener("click", function(e) {
-        e.preventDefault();
-        logout_box.style.display = "none";
-        create_subject_box.style.display = "none";
-        update_subject_box.style.display = "none";
-        delete_box.style.display = "none";
-    });
-});
-
-
 // 수강 과목
-const subject = {
+const subjectData = {
     "data" : [
         {
             "subjectName" : "영양갱 경제학",
@@ -147,15 +34,120 @@ const subject = {
     ]
 }
 
-// const subjectData = document.getElementById("data");
+function create_subject() {
+    let html = "";
 
-// function createSubject() {
-//     for (subjectList of subject) {
-//         // const subjectList = document.createElement("li");
-//         // subjectList.innerHTML = `${subjectList.subjectName}`;
-//         // subjectData.appendChild(subjectList);
-//         let li = document.createElement('li');
-//         li.textContent = `${subjectList.subjectName}`;
+    for (let sd of subjectData.data) {
+        html += `
+        <ul class="data">
+        <li class="list">${sd.subjectName}</li>
+        <li class="list">${sd.professor}</li>
+        <li class="list">${sd.credit}</li>
+        <li class="list">${sd.subjectType}</li>
+        <li><button class="setting">설정</button></li>
+        </ul>
+        `
+    }
+    document.getElementById("subjectList").innerHTML = html;
+}
+create_subject();
+
+// 로그아웃 버튼 클릭 시 이벤트 발생
+const logout = document.getElementById("logout");
+const logout_box = document.getElementById("logout_box");
+
+logout.addEventListener("click", function(e) {
+    e.preventDefault();     // 기본 동작을 실행하지 않도록 지정
+    logout_box.style.display = "block";     // logout_box의 스타일을 none에서 block으로 변경
+    
+});
+
+// 수강 과목 + 버튼 클릭 시 이벤트 발생
+const plus = document.getElementById("plus");
+const create_subject_box = document.getElementById("create_subject_box");
+
+plus.addEventListener("click", function(e) {
+    e.preventDefault();
+    create_subject_box.style.display = "block";     // logout_box의 스타일을 none에서 block으로 변경
+});
+
+// 수강 과목 생성에서 생성 버튼 클릭 시 이벤트 발생
+const create = document.querySelector(".create");                           // 생성 버튼
+const subject_container = document.getElementById('subject_container');     // 생성 버튼 클릭 시 ul이 추가될 div
+const Ul = subject_container.querySelector('.data');                        // data 클래스 가져오기
+const list = document.querySelectorAll(".list");
+
+create.addEventListener("click", function(e) {
+    e.preventDefault();
+    const newUl = document.createElement('ul');         // 새로운 ul 태그 생성
+    newUl.className = 'data';                           // 새로 생긴 ul 태그는 data 클래스
+    const newLi = Ul.cloneNode(true);                   // Ul(data) 노드 복사, true라서 하위 노드까지 모두 복사
+    create_subject_box.style.display = "none";
+    subject_container.appendChild(newUl);               // subject_container div에 새로운 ul 태그 추가
+    newUl.appendChild(newLi);                           // 새로 추가된 ul에 복제된 노드 추가
+});
+
+
+// 수강 과목 설정 버튼 클릭 시 이벤트 발생
+const setting = document.querySelectorAll(".setting");
+const setting_button = document.querySelector(".setting_button");
+
+for(let i = 0; i < setting.length; i++) {
+        setting.addEventListener("click", function(e) {
+        
+        })
+    }
+
+// setting.addEventListener("click", function(e) {
+//     const clickX = e.clientX;
+//     const clickY = e.clientY;
+
+//     setting_button.style.left = clickX + "px";
+//     setting_button.style.top = clickY + "px";
+//     setting_button.style.display = "block";
+// });
+
+// setting_button.addEventListener("click", function(e) {
+//     e.stopPropagation();
+//     setting_button.style.display = "none";
+// });
+
+// // // 설정 버튼이랑 설정 버튼 클릭 시 나오는 레이어 아닌 부분 클릭 시 설정 레이어 사라지는 이벤트
+// document.addEventListener("click", function(e) {
+//     if(e.target !== setting && e.target !== setting_button) {
+//         setting_button.style.display = "none";
 //     }
-// }
-// createSubject();
+// })
+
+
+// 수정 버튼 클릭 시 이벤트 발생
+const update_button = document.querySelector(".update_button");
+const update_subject_box = document.getElementById("update_subject_box");
+
+update_button.addEventListener("click", function(e) {
+    e.preventDefault();
+    update_subject_box.style.display = "block";       // update_subject_box의 스타일을 none에서 block으로 변경
+});
+
+// 제거 버튼 클릭 시 이벤트 발생
+const delete_button = document.querySelector(".delete_button");
+const delete_box = document.getElementById("delete_box");
+
+delete_button.addEventListener("click", function(e) {
+    e.preventDefault();
+    delete_box.style.display = "block";
+});
+
+
+// 로그아웃 취소, 수강 과목 생성 취소 버튼 클릭 시 이벤트 발생
+let cancel = document.querySelectorAll(".cancel");
+
+cancel.forEach((Element) => {
+    Element.addEventListener("click", function(e) {
+        e.preventDefault();
+        logout_box.style.display = "none";
+        create_subject_box.style.display = "none";
+        update_subject_box.style.display = "none";
+        delete_box.style.display = "none";
+    });
+});
